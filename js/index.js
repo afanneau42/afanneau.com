916f574782c103bb15e190e9ab76d5406ddc7c15
afanneau <afanneau@student.42.fr>
2018-03-19 20:05:58 +0100

    second slide almost done + first slide rectif + side dot navigation

--- js/index.js
+++ js/index.js
@@ -12,11 +12,31 @@ $(document).ready(() => {
     }
     $(this).scrollTop(0);
     
-    // $(window).scroll(() => {
-    //     let yPos = $(window).scrollTop()
-    //     console.log( 20 - (yPos / $('body').height()) * 20)
-    //     $('body').attr('style', 'background-color: hsl(0, 0%, '+ (15 -(yPos / $('body').height()) * 15) +'%)')
-    // })
+    $(window).scroll(() => {
+        let yPos = $(window).scrollTop()
+        // console.log('yPos: ' + yPos)
+        // console.log('offset: ' + $('body').height())        
+        // console.log('offset: ' + $('#second-div').offset().top)
+        if (yPos >= $('#third-div').offset().top - ($('#third-div').height() * 0.50)) {
+            $('.active').removeClass('active');
+            $('#li3').parent().addClass('active');
+            $('#resume_div').removeClass('introduced');
+            console.log('trig 3')
+        }
+        else if (yPos >= $('#second-div').offset().top - ($('#second-div').height() * 0.50)) {
+            $('.active').removeClass('active');
+            $('#li2').parent().addClass('active');
+            $('#resume_div').addClass('introduced');
+            console.log('trig 2')
+        }
+        else if (yPos < $('#second-div').offset().top - ($('#second-div').height() * 0.50)) {
+            $('.active').removeClass('active');
+            $('#li1').parent().addClass('active');
+            $('#resume_div').removeClass('introduced');
+            console.log('trig 1')
+        }
+      
+    })
     $('#introduce').click(() => {
         goToByScroll('second-div')
     })
@@ -28,4 +48,19 @@ $(document).ready(() => {
     //   btn.style.setProperty('--x', x + 'px')
     //   btn.style.setProperty('--y', y + 'px')
     // }
+
+    $('.dots li').click(function(){
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('#li1').click(function() {
+        goToByScroll('first-div')
+    })
+    $('#li2').click(function() {
+        goToByScroll('second-div')
+    })
+    $('#li3').click(function() {
+        goToByScroll('third-div')
+    })
 })
